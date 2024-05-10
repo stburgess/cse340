174b3f48045531d31f0397cbc252a905564ac8f0
stburgess <stephentburgess@aim.com>
2024-05-11 01:34:06 +0300

    restore error functions and css styling

--- server.js
+++ server.js
@@ -27,23 +27,18 @@ app.set("layout", "./layouts/layout") // not at views root
  *************************/
 app.use(static)
 //Index route
-/*
 app.get("/", utilities.handleErrors(baseController.buildHome))
-*/
-app.get("/", baseController.buildHome)
 //Inventory routes
 app.use("/inv", inventoryRoute)
 // File Not Found Route - must be last route in list
-/*
 app.use(async (req, res, next) => {
   next({status: 404, message: 'Sorry, we appear to have lost that page!'})
 })
-*/
+
 /* ***********************
 * Express Error Handler
 * Place after all other middleware
 *************************/
-/*
 app.use(async (err, req, res, next) => {
   let nav = await utilities.getNav()
   console.error(`Error at: "${req.originalUrl}": ${err.message}`)
@@ -54,7 +49,7 @@ app.use(async (err, req, res, next) => {
     nav
   })
 })
-*/
+
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
