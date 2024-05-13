9bbd26d30355cb3a251b94d46311409afd96d561
stburgess <stephentburgess@aim.com>
2024-05-13 23:59:59 +0300

    added error handling for assign 3

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -2,10 +2,11 @@
 const express = require("express")
 const router = new express.Router() 
 const invController = require("../controllers/invController")
+const utilities = require("../utilities")
 
 // Route to build inventory by classification view
-router.get("/type/:classificationId", invController.buildByClassificationId);
+router.get("/type/:classificationId", utilities.handleErrors(invController.buildByClassificationId));
 // Route to build detail by detail view
-router.get("/detail/:invId", invController.buildByInvId);
+router.get("/detail/:invId", utilities.handleErrors(invController.buildByInvId));
 
 module.exports = router;
